135f1f745acba733e365e16a08ddbf20c12a7573
dkdmlk <dkdmlk@naver.com>
2022-04-11 12:30:58 +0900

    추가

--- 4.11/SQL/init.sql
+++ 4.11/SQL/init.sql
@@ -45,3 +45,73 @@ INSERT INTO customer_history_purchase(customer_id, cost, create_at) VALUES('2286
 INSERT INTO customer_history_purchase(customer_id, cost, create_at) VALUES('20381000',5000, '2022-01-03');
 INSERT INTO customer_history_purchase(customer_id, cost, create_at) VALUES('20381000',8000, '2022-01-03');
 INSERT INTO customer_history_purchase(customer_id, cost, create_at) VALUES('20381000',6600, '2022-01-03');
+
+-- 1. 탈퇴하지 않은 고객 아이디와 이름, 등급 조회.
+select
+	customer_id ,
+	grade
+from customer 
+where is_use
+
+-- 2. '2021-12-09' 이후에 가입한 고객 수 조회.
+select 
+count(*) as '가입고객수'
+from customer c 
+where create_at > 2021-12-09
+
+-- 3. 구매이력이 있는 고객 아이디와, 이름, 구매날짜, 비용 조회.
+select
+c.customer_id as '고객 아이디',
+c.customer_name as '이름',
+chp.create_at as '구매날짜', 
+chp.cost as '비용'
+from customer as c 
+inner join customer_history_purchase as chp
+on c.customer_id = chp.customer_id
+
+ 
+
+
+
+-- 4. 등록된 고객 중 한 번도 구매 이력이 없는 고객 아이디, 이름, 회원 탈퇴 여부 조회.
+select 
+c.customer_id ,
+c.customer_name ,
+c.is_use
+from customer as c 
+left join customer_history_purchase as chp
+on c.customer_id = chp.customer_id
+
+
+
+-- 5. '2022-01-03'기준 고객 아이디별 구매 건수와 총 비용, 이름, 등급 조회.
+select 
+	c.customer_id ,
+	count(chp.history_no),
+	sum(cost),
+	c.customer_name ,
+	c.grade 
+from customer as c 
+inner join customer_history_purchase as chp
+on c.customer_id = chp.customer_id 
+where chp .create_at > 2022-01-03
+group by chp.customer_id
+
+
+-- 6. '2022-01-03'기준 3번 이상 구매한 고객 아이디, 이름, 등급 조회.
+select 
+	c.customer_id ,
+	count(chp.history_no),
+	sum(cost),
+	c.customer_name ,
+	c.grade 
+from customer as c 
+inner join customer_history_purchase as chp
+on c.customer_id = chp.customer_id 
+where chp .create_at > 2022-01-03
+group by chp.customer_id
+having count(chp.history_no)  >= 3
+
+
+-- 7. 고객번호가 5194998인 고객 등급을 골드로 업데이트 하시오.
+UPDATE customer SET grade = '골드' WHERE customer_id = 5194998;
